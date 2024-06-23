ed2308330620998179318e2a0ed49f6f7e753eac
Maxim Gadelshin <gadelshin.maksimm@yandex.ru>
2024-06-23 22:10:03 +0400

    commit 23.06

--- src/js/index.js
+++ src/js/index.js
@@ -8,39 +8,34 @@ import Swiper from 'swiper/bundle';
 // import styles bundle
 import 'swiper/css/bundle';
 
-const swiper = new Swiper('.swiper__services', {
-	// Optional parameters
+const swiperServices = new Swiper('.swiper__services', {
 	direction: 'horizontal',
 	loop: true,
 	looperSlides: 3,
 	slidesPerView: 3,
 	centeredSlides:true,
 	speed:1000,
-
 	effect: 'creative',
-	
 	creativeEffect: {
 		limitProgress: 3,
 		prev: {
-			// shadow: true,
 			translate: ["-100%", -115, 0],
 		},
 		next: {
-			// shadow: true,
 			translate: ["100%", -115, 0],
 		},
 	},
 	breakpoints: {
 		100: {
-		slidesPerView:1,
-		creativeEffect: {
-			prev:{
-				translate: ["-100%", -24, 0],
+			slidesPerView:1,
+			creativeEffect: {
+				prev:{
+					translate: ["-100%", -24, 0],
+				},
+				next: {
+					translate: ["100%", -24, 0],
+				},
 			},
-			next: {
-				translate: ["100%", -24, 0],
-			},
-		},
 		},
 		500: {
 			slidesPerView:2,
@@ -52,9 +47,8 @@ const swiper = new Swiper('.swiper__services', {
 				next: {
 					translate: ["100%", 0, 0],
 				},
-				
 			},
-			},
+		},
 		768: {
 			slidesPerView:3,
 			centeredSlides: true,
@@ -87,17 +81,83 @@ const swiper = new Swiper('.swiper__services', {
 				},
 			},
 		}
-			
 },
-	// If we need pagination
 	pagination: {
 	  el: '.swiper-pagination__services',
 	  clickable: true,
 	},
- 
 	navigation: {
 	  nextEl: '.swiper-button-next',
 	  prevEl: '.swiper-button-prev',
 	},
+ });
 
+
+ const swiperExamplesBefore = new Swiper ('.examples__before', {
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 1,
+	speed:600,
+	allowTouchMove: false,
+	effect: "creative",
+	creativeEffect: {
+	  prev: {
+		 shadow: true,
+		 translate: [0, 0, -400],
+	  },
+	  next: {
+		 translate: ["100%", 0, 0],
+	  },
+	},
+	pagination: {
+		el: '.examples__pagination',
+		clickable: true,
+	 },
+	 navigation: {
+		nextEl: '.examples__btn-next',
+		prevEl: '.examples__btn-prev',
+	 },
+ });
+
+ const swiperExamplesAfter = new Swiper ('.examples__after', {
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 1,
+	speed:600,
+	allowTouchMove: false,
+	effect: "creative",
+	creativeEffect: {
+	  prev: {
+		 shadow: true,
+		 translate: [0, 0, -400],
+	  },
+	  next: {
+		 translate: ["100%", 0, 0],
+	  },
+	},
+	pagination: {
+		el: '.examples__pagination',
+		clickable: true,
+	 },
+	 navigation: {
+		nextEl: '.examples__btn-next',
+		prevEl: '.examples__btn-prev',
+	 },
+ });
+
+ const swiperExamplesDesc = new Swiper ('.examples__desc', {
+	direction: 'horizontal',
+	loop: true,
+	slidesPerView: 1,
+	speed:800,
+	allowTouchMove: false,
+	effect: "flip",
+	pagination: {
+		el: '.examples__pagination',
+		clickable: true,
+	 },
+	 navigation: {
+		nextEl: '.examples__btn-next',
+		prevEl: '.examples__btn-prev',
+	 },
  });
